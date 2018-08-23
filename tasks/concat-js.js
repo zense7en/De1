6de3c2ad05e454f048ca0b213aba5f2e251c78fa
= <=>
2018-08-24 05:16:07 +0700

    update

---
+++ tasks/concat-js.js
@@ -0,0 +1,13 @@
+module.exports = function (gulp, $, browserSync) {
+	gulp.task('concat-js', function () {
+		return gulp.src([
+				'bower_components/jquery/dist/jquery.slim.min.js',
+				'bower_components/popper.js/dist/umd/popper.min.js',
+				'bower_components/bootstrap/dist/js/bootstrap.min.js',
+				// Phần Plugins
+				'bower_components/owl.carousel/dist/owl.carousel.min.js',
+			])
+			.pipe($.concat('thuvien.js'))
+			.pipe(gulp.dest('./dist/js'));
+	});
+};
